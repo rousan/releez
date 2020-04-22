bcf8d578847cbe85c148545ff18f25f08805fec5
Rousan Ali <rousanali786@gmail.com>
2020-04-22 21:39:40 +0530

    Add version arg

--- src/main.rs
+++ src/main.rs
@@ -18,13 +18,21 @@ fn main() {
                 .takes_value(true)
                 .required(false),
         )
+        .arg(
+            Arg::with_name("releaseVersion")
+                .help("The next release version")
+                .value_name("RELEASE_VERSION")
+                .index(1)
+                .required(true),
+        )
         .get_matches();
 
     let config_file_path = matches
         .value_of("config")
         .unwrap_or(constants::DEFAULT_CONFIG_FILE_PATH);
+    let release_version = matches.value_of("releaseVersion").unwrap();
 
-    if let Err(err) = releez::runner::run_release_checklist(config_file_path) {
+    if let Err(err) = releez::runner::run_release_checklist(config_file_path, release_version) {
         eprintln!("{} {}", "error:".red(), err);
     }
 }
--- src/runner/mod.rs
+++ src/runner/mod.rs
@@ -1,12 +1,13 @@
 use crate::prelude::*;
 use std::path::Path;
 
-pub fn run_release_checklist(config_file_path: &str) -> crate::Result<()> {
+pub fn run_release_checklist(config_file_path: &str, release_version: &str) -> crate::Result<()> {
     let config_file_path = Path::new(config_file_path)
         .canonicalize()
         .context(format!("Config file path does not exist: {}", config_file_path))?;
 
-    // println!("{:?}", config_file_path);
+    println!("{:?}", config_file_path);
+    println!("{}", release_version);
 
     Ok(())
 }
